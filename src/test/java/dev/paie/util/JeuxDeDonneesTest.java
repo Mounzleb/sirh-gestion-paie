bc34441c7b1a09876162b4c274700254864a3583
Mounzer HADDARA <mounzer.haddara@hotmail.fr>
2017-05-31 13:27:50 +0200

    TP Spring test_entreprise ok

--- src/test/java/dev/paie/util/JeuxDeDonneesTest.java
+++ src/test/java/dev/paie/util/JeuxDeDonneesTest.java
@@ -46,33 +46,33 @@ public class JeuxDeDonneesTest {
 		));
 
 	}
-//
-//	@Test
-//
-//	public void test_employe() {
-//
-//		assertThat(bulletin1.getRemunerationEmploye().getMatricule(), equalTo("M01"));
-//
-//	}
 
-//	@Test
-//
-//	public void test_entreprise() {
-//
-//		assertThat(bulletin1.getRemunerationEmploye().getEntreprise().getSiret(),
-//
-//				equalTo("80966785000022"));
-//
-//		assertThat(bulletin1.getRemunerationEmploye().getEntreprise().getDenomination
-//
-//		(), equalTo("Dev Entreprise"));
-//
-//		assertThat(bulletin1.getRemunerationEmploye().getEntreprise().getCodeNaf(),
-//
-//				equalTo("6202A"));
-//
-//	}
-//
+	@Test
+
+	public void test_employe() {
+
+		assertThat(bulletin1.getRemunerationEmploye().getMatricule(), equalTo("M01"));
+
+	}
+
+	@Test
+
+	public void test_entreprise() {
+
+		assertThat(bulletin1.getRemunerationEmploye().getEntreprise().getSiret(),
+
+				equalTo("80966785000022"));
+
+		assertThat(bulletin1.getRemunerationEmploye().getEntreprise().getDenomination
+
+		(), equalTo("Dev Entreprise"));
+
+		assertThat(bulletin1.getRemunerationEmploye().getEntreprise().getCodeNaf(),
+
+				equalTo("6202A"));
+
+	}
+
 //	@Test
 //
 //	public void test_cotisationsNonImposables() {
