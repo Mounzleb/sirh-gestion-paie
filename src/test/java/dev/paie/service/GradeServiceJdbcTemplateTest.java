88f33bb3f2dec1b9334ec28e999d44e7cf821d0e
Mounzer HADDARA <mounzer.haddara@hotmail.fr>
2017-06-01 12:18:34 +0200

    commit bis du précédent : remarque de bean et componentscann dans la classe GradeServiceJdbcTemplateTest

--- src/test/java/dev/paie/service/GradeServiceJdbcTemplateTest.java
+++ src/test/java/dev/paie/service/GradeServiceJdbcTemplateTest.java
@@ -14,7 +14,8 @@ import dev.paie.config.ServicesConfig;
 import dev.paie.entite.Grade;
 import dev.paie.spring.DataSourceMySQLConfig;
 
-@ContextConfiguration(classes = { ServicesConfig.class, JeuxDeDonneesConfig.class, DataSourceMySQLConfig.class})
+// ici j'ai choisie mes deux bean dont j'ai besoin et non pas les class ou il y'avait des ComponentScann et donc mon programme avait beaucoup de bean qui ne serve à rien pour ma classe ici présente 
+@ContextConfiguration(classes = { GradeServiceJdbcTemplate.class, DataSourceMySQLConfig.class })
 @RunWith(SpringRunner.class)
 public class GradeServiceJdbcTemplateTest {
 
@@ -24,32 +25,30 @@ public class GradeServiceJdbcTemplateTest {
 	@Test
 	public void test_sauvegarder() {
 		Grade saveGrade = new Grade();
-		
-		
+
 		saveGrade.setId(1);
 		saveGrade.setCode("code essaie 1");
 		saveGrade.setNbHeuresBase(new BigDecimal("200.0"));
 		saveGrade.setTauxBase(new BigDecimal("0.8"));
-		
-		
-		gradeService.sauvegarder(saveGrade );
+
+		gradeService.sauvegarder(saveGrade);
 
 	}
-//	@Test
-//	public void test_lister() {
-//
-//	}
+
+	// @Test
+	// public void test_lister() {
+	//
+	// }
 	@Test
 	public void test_mettre_a_jour() {
-		
+
 		Grade updateGrade = new Grade();
-		
+
 		updateGrade.setId(2);
 		updateGrade.setCode(UUID.randomUUID().toString());
 		updateGrade.setNbHeuresBase(new BigDecimal("300.0"));
 		updateGrade.setTauxBase(new BigDecimal("0.6"));
-		
-		
+
 		gradeService.mettre_a_jour(updateGrade);
 
 	}
